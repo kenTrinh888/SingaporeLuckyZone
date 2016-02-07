a12b9285d6b9e6b8b96980b09172b87d1f5076df
kenTrinh <hdk.trinh.2012@smu.edu.sg>
2016-02-07 16:29:05 +0800

    port fixed

--- app.js
+++ app.js
@@ -282,5 +282,6 @@ process.on('uncaughtException', function (err) {
 //     res.send(result);
 // });
 
-app.listen(3000);
-console.log("Running at Port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
